91ed44e7bd0778fd4580ff33e84f93c68c68be28
Muntaz Kaleem <mkhb654@users.noreply.github.com>
2022-06-06 00:04:42 -0400

    added redux toolkit

--- src/components/ Homepage.jsx
+++ src/components/ Homepage.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import millify from 'millify';
+import {Typography, Row, Col, Statistic} from 'antd'
+import { Link } from 'react-router-dom';
 
-function  Homepage() {
+import { useGetCryptosQuery } from '../services/cryptoApi';
+
+const {Title} = Typography;
+const  Homepage = () => {
+  const { data, isFetching } = useGetCryptosQuery(10);
+  const globalStats = data?.data?.stats;
+
+  if (isFetching) return <Loader />;
   return (
-    <div>
-      
-    </div>
+    <>
+     <Title level={2} className="heading">Global Crypto Stats</Title>
+      <Row gutter={[32, 32]}>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+        <Col span={12}><Statistic title="Total Market Cap:" value={`$${millify(globalStats.totalMarketCap)}`} /></Col>
+        <Col span={12}><Statistic title="Total 24h Volume" value={`$${millify(globalStats.total24hVolume)}`} /></Col>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+        <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
+      </Row>
+      </>
   )
 }
 
-export default  Homepage
+export default  Homepage;
