c83ecef50b610a9fa4db5fa6551b50d01efcc47b
Muntaz Kaleem <mkhb654@users.noreply.github.com>
2022-06-05 23:41:11 -0400

    navigation bar works

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,16 +5,29 @@ import {HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuFold
 import icon from '../images/cryptocurrency.png';
 
 const NavBar = () => {
-  return (
-    <div className="nav-container">
-      <div className="logo-container">
-          <Avatar src={icon} size="large"/>
-          <Typography.Title level={2} className="logo">
-          <link to="/">CryptoVerse</link>
-          </Typography.Title>     
+    return (
+        <div className="nav-container">
+          <div className="logo-container">
+            <Avatar src={icon} size="large" />
+            <Typography.Title level={2} className="logo"><Link to="/">Cryptoverse</Link></Typography.Title>
+          <Menu theme="dark">
+            <Menu.Item icon={<HomeOutlined />}>
+              <Link to="/">Home</Link>
+            </Menu.Item>
+            <Menu.Item icon={<FundOutlined />}>
+              <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+            </Menu.Item>
+            <Menu.Item icon={<MoneyCollectOutlined />}>
+              <Link to="/exchanges">Exchanges</Link>
+            </Menu.Item>
+            <Menu.Item icon={<BulbOutlined />}>
+              <Link to="/news">News</Link>
+            </Menu.Item>
+          </Menu>
         </div>
-     </div>  
-  )
-}
+        </div>
+      );
+    };
+
 
 export default NavBar;
